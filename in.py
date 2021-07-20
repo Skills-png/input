5c55bacc9491cb485899091455a0132d111426a2
loadercc <60961630+Skills-png@users.noreply.github.com>
2021-07-20 14:05:02 +0500

    Update in.py

--- in.py
+++ in.py
@@ -1,13 +1,15 @@
-import socket, threading, time, bs4, requests 
-key = 8194
+import socket, threading, time #импорт библиотек
+import bs4, requests #это для ip
+import os as q#ПОдрубаем библиотеку os 
+key = 8194#переменая с ключем для шифровки данных ну это мы потом посмотрим 
+ips = socket.gethostbyname(socket.gethostname())
 shutdown = False
 join = False
-host = socket.gethostbyname(socket.gethostname())
-server = (socket.gethostbyname(socket.gethostname()), 9090)
-s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.bind((host, 0))
-s.setblocking(0) 
-def receving(name, sock):
+col = 0
+max = 10
+#----------------------------НАЧАЛО----------------------------
+#позволяет принимать данные другово пользователя 
+def receving (name, sock):
 	while not shutdown:
 		try:
 			while True:
@@ -20,23 +22,40 @@ def receving(name, sock):
 					elif k == False or i == " ":
 						decrypt += i
 					else:
-						decrypt += chr(ord(i)^key)			
+						decrypt += chr(ord(i)^key)
+#----------------------------КОНЕЦ----------------------------				
 				print(decrypt)
 				time.sleep(0.2)
 		except:
 				pass
-rT = threading.Thread(target = receving, args = ("RecvThread",s))
-rT.start()
-while shutdown == False:
-	if join == False:
-		join = True
+host = socket.gethostbyname(socket.gethostname())#переменая которая содержит ip  
+port = 0#почему port равен 0 все очень просто клиент будет лишь подключатся к этой сети он не будет её создавать ну как-то так. 
+server = (ips,9090)#указываем порт на котором он поднят
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)#я уже говорил 
+s.bind((host,port))#делаем функцию bind которая будет поключатся к host и port 
+s.setblocking(0)#ААААААААА ЭТО ЭТО ПРОСТО О. она нужна для того чтобы не выдовало каких либо ошибок либо это не все  
+alias = input("Имя: ")#Вводим значения для переменой alias которая будет содержать имя нашего пользователя и имя как имя
+rT = threading.Thread(target = receving, args = ("RecvThread",s))#многопоточность 
+rT.start()#запускаем нашу многопоточность вот блин слишком много я чо то объясняю всеровно не кому не чего не понятно
+while shutdown == False:#пока клиент не вышел дулаются ледуюшие действия
+	if join == False:#если пользователь не присоеденен то отправляется следующие вот тут снизу 
+		s.sendto(("[" +alias + "] Вошёл в чат").encode("utf-8"),server)#отправляется сообщение на сервер то что "alias" присоединился к серверу 
+		join = True#это то что подключился
 	else:
 		try:
-			message = input()
-			if message != "":
-				s.sendto((message).encode("utf-8"),server)
-			time.sleep(0.2)
+			message = input()#ввод сообщения самое основное потом мы его будем шинковать
+			#а вот тут мы зашифровываем сообщение --НАЧАЛО--
+			#---------------------------------------Это я по фикчу пв по фикшу о        
+			#crypt = ""
+			#for i in message:
+			#	crypt += chr(ord(i)^key)
+			#message = crypt
+			#--КОНЕЦ--
+			if message != "":#а тут этот для того чтобы не отправлять пустое сообщение (Здесь мог бы баг)
+				s.sendto(("["+alias+"] " + message).encode("utf-8"),server)
+			time.sleep(0.2)#(задержка)диопозон между отправкой и получением 
 		except:
-			shutdown = True
+			s.sendto(("["+alias+"] Покинул чат").encode("utf-8"),server)#сообщение которое отправляется на сервер о том что пользователь покинул чат
+			shutdown = True#закрываем соединение 
 rT.join()
 s.close()
