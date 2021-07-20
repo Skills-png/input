5f6575f1079bf692e31c5e8bcf249ae13758b833
loadercc <60961630+Skills-png@users.noreply.github.com>
2021-07-20 14:13:48 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,25 +1,28 @@
-import socket, time  
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import socket
+import time
 host = socket.gethostbyname(socket.gethostname())
-port = 9090  
-clients = []  
-s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
-s.bind((host,port)) 
-quit = False 
- 
-while not quit:  
-	try:
-		data, addr = s.recvfrom(1024)  
+port = 9090
+clients = []
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((host, port))
+quit = False
 
-		if addr not in clients:  
-			clients.append(addr) 
-		itsatime = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime()) 
-		print("["+addr[0]+"]=["+str(addr[1])+"]=["+itsatime+"]/",end="") 
-		print(data.decode("utf-8")) 
-		for client in clients: 
-			if addr != client:
-				s.sendto(data,client)
- 
-	except: 
-		 
-		quit = True#переменая quit присваевается значение True
+while not quit:
+    try:
+        (data, addr) = s.recvfrom(1024)
+
+        if addr not in clients:
+            clients.append(addr)
+        itsatime = time.strftime('%Y-%m-%d-%H.%M.%S', time.localtime())
+        print '[' + addr[0] + ']=[' + str(addr[1]) + ']=[' + itsatime \
+            + ']/'
+        print data.decode('utf-8')
+        for client in clients:
+            if addr != client:
+                s.sendto(data, client)
+    except:
+
+        quit = True
 s.close()
